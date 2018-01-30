15cefc5ce25d9c0c021aeb8e9ad7412787f62b7b
Fredrik Lundqvist <zeb75@hotmail.com>
2018-01-30 13:12:58 +0100

    v1.0

---
+++ Identity/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Identity.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Person> Persons { get; set; }
+
+        public List<City> Cities { get; set; }
+    }
+}
---
+++ Identity/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Identity.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Country Country { get; set; }
+    }
+}
